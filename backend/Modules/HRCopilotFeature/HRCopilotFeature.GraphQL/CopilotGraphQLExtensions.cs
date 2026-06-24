af9e0356f0414804f4adcc6431f0b0ca1bf641b6
Vinayak3512 <2.5393479e+08+Vinayak3512@users.noreply.github.com>
2026-06-24 18:19:24 +0000

    Fix: Rename GraphQL registration for HRCopilot to match project pattern and resolve IRequestExecutorBuilder error

--- backend/Modules/HRCopilotFeature/HRCopilotFeature.GraphQL/CopilotGraphQLExtensions.cs
+++ backend/Modules/HRCopilotFeature/HRCopilotFeature.GraphQL/CopilotGraphQLExtensions.cs
@@ -2,7 +2,7 @@ using HotChocolate.Execution.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 namespace HRCopilotFeature.GraphQL
 {
-    public static class GraphQLModuleRegistration
+    public static class CopilotGraphQLExtensions
     {
         public static IRequestExecutorBuilder AddCopilotGraphQL(this IRequestExecutorBuilder builder)
         {
